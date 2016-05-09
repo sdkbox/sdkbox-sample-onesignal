099fef07650c495d4a9cc746a4de04e48bc0b9f5
yinjimmy <jimmy.yin5@gmail.com>
2016-05-09 23:50:55 +0800

    update Js sample

--- js/src/app.js
+++ js/src/app.js
@@ -24,6 +24,7 @@ var HelloWorldLayer = cc.Layer.extend({
     createTestMenu:function() {
         var self = this;
         self.postNotificationTest = function() {}
+        self.userId = "4db7cb0f-daca-49d6-b3dd-a96774de9f72";
 
         var menu = new cc.Menu();
 
@@ -58,7 +59,8 @@ var HelloWorldLayer = cc.Layer.extend({
             onGetTags :function (jsonString) { cc.log("onGetTags tags=%s", jsonString); },
             onIdsAvailable :function (userId,  pushToken) {
                 cc.log("onIdsAvailable userId=%s, pushToken=%s", userId, pushToken);
-                self.item2.setString(userId)
+                self.userId = userId;
+                self.item2.setString(userId);
             },
             onPostNotification :function (success,  message) { cc.log("onPostNotification success=%s, message=%s", success ? "yes" : "no", message); },
             onNotification :function (isActive,  message, additionalData) { cc.log("onNotification isActive=%s, message=%s, additionalData=%s", isActive?"yes":"no", message, additionalData); }
@@ -80,7 +82,7 @@ var HelloWorldLayer = cc.Layer.extend({
                     "en": "Js test message"
                 },
                 "include_player_ids": [
-                    "4db7cb0f-daca-49d6-b3dd-a96774de9f72",
+                    self.userId,
                     "33ad9d96-df00-42a2-b5ab-73417f777d42",
                     "5ea075c5-016d-4f55-891c-3b6c4b393e49"
                 ],
